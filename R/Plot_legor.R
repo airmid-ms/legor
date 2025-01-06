dc15d7990d8d51a08b060b3985321808a47a518b
RobertCoffey01 <robert.coffey.2022@mumail.ie>
2025-01-06 20:58:57 +0000

    Updated Plot function: changed the look of the graph, also changed part of the fit to use %>% intead of |> and also in the function n was set to 200 instead of 100.

--- R/Plot_legor.R
+++ R/Plot_legor.R
@@ -1,16 +1,14 @@
-# plot.legor_fit()
+plot.lego_fit <- function(x, pieces_grid = pretty(x$data$pieces, n = 200), ...) {
 
-plot.lego_fit <- function(x, pieces_grid = pretty(x$data$pieces, n = 100), ...) {
-
-  # Check that the input is a lego_fit object
+  ## Check that the input is a lego_fit object
   if (!inherits(x, "lego_fit")) {
     stop("The input must be a 'lego_fit' object.")
   }
 
-  # Extract the data
+  ## Extract the data
   df <- x$data
 
-  # Generate predicted values based on the pieces grid and the fit_type
+  ## Generate predicted values based on the pieces grid and the fit_type
   fits <- switch(x$fit_type,
                  lm = {
                    tibble(pieces = pieces_grid, pred = predict(x$model,
@@ -18,34 +16,33 @@ plot.lego_fit <- function(x, pieces_grid = pretty(x$data$pieces, n = 100), ...) 
                  },
                  loess = {
                    tibble(pieces = pieces_grid, pred = predict(x$model,
-                                                               newdata = tibble(pieces = pieces_grid))) |> na.omit()
+                                                               newdata = tibble(pieces = pieces_grid))) %>%
+                     na.omit()
                  },
                  polynomial = {
                    tibble(pieces = pieces_grid, pred = predict(x$model,
                                                                newdata = tibble(pieces = pieces_grid)))
                  })
-
+  ## Find the top 3 highest retail price lego sets.
   top3_df <- df %>%
-    arrange(desc(us_retailprice)) %>%  # Sort by 'us_retailprice' in descending order
-    head(3)                           # Get the top 3 rows
+    arrange(desc(us_retailprice)) %>%
+    head(3)
 
-  # Create the plot with labels for the highest 3 points based on 'us_retailprice'
+  ## Create the plot with labels for the highest 3 points based on 'us_retailprice'
   ggplot(df, aes(x = pieces, y = us_retailprice)) +
     geom_point(aes(colour = us_retailprice)) +
-    theme_bw() +
+    theme_linedraw() +
     xlab("Number of Pieces") +
     ylab("US Retail Price") +
-    ggtitle(paste(x$fit_type, "Fit based on", x$data_type, "data")) +
+    ggtitle(paste(x$fit_type, "Fit based on lego_data")) +
     geom_line(data = fits, aes(x = pieces, y = pred, colour = pred)) +
-    geom_text(data = top3_df,         # Use the top 3 highest points
-              aes(label = name),      # Label using the 'name' column
-              vjust = -0.5,           # Adjust vertical position
-              colour = "black") +     # Label color
-    theme(legend.position = "none") +
-    scale_color_viridis_c()
+    geom_text(data = top3_df,
+              aes(label = name),
+              vjust = -0.5) +
+    scale_color_viridis_c(option = "inferno")
 }
 
 ## Examples
-plot(fit_result)
-plot(fit_result2)
-plot(fit_result3)
+plot(model)
+plot(model2)
+plot(model3)
