89a3a1432e557b70f3a56aac37fef379424cc78f
achurch1 <aaron.church.2021@mumail.ie>
2025-01-08 17:23:22 +0000

    Added Roxygen

--- R/lego_fit08:01.R
+++ R/lego_fit08:01.R
@@ -1,8 +1,49 @@
+#' Fit LEGO Data Models
+#'
+#' This package provides functions to fit various models to LEGO datasets, including linear regression (\code{"lm"}), LOESS, and polynomial regression.
+#'
+#' ## Define the Generic Function
+#'
+#' The \code{fit()} function is a generic function for fitting models to LEGO data. It calls the appropriate method based on the class of the object passed to it.
+#'
+#' @param obj A data frame containing LEGO data. It must include columns such as \code{pieces} and \code{us_retailprice}.
+#' @param ... Additional arguments passed to specific methods (e.g., \code{fit_type}, \code{polynomial_degree}).
+#'
+#' @return An object of class \code{"lego_fit"} which contains the fitted model, input data, and the fit type used.
+#'
+#' @export
 fit <- function(obj, ...) {
   UseMethod("fit")
 }
 
 
+#' Fit LEGO Model Using Different Methods
+#'
+#' Fits a specified model to LEGO data using linear regression (\code{"lm"}), LOESS, or polynomial regression.
+#'
+#' @param obj A data frame containing LEGO data. It must include columns \code{pieces} and \code{us_retailprice}.
+#' @param fit_type A character string specifying the type of fit to apply. Choices are: \code{"lm"}, \code{"loess"}, or \code{"polynomial"}. Default is \code{"lm"}.
+#' @param polynomial_degree A numeric value specifying the degree of the polynomial for polynomial regression. Default is \code{3}. Must be greater than or equal to \code{1} for polynomial regression.
+#' @param year_range A numeric vector of length 2 specifying the year range the user wants to filter by, years must be written in ascending order. this parameter does not need to be specified if the user does not wish to filter by a specific range of years.
+#' @param ... Additional arguments (currently not being used) saved for future methods or extensions.
+#' @details
+#' The function allows users to fit linear models (\code{"lm"}), local polynomial regression (\code{"loess"}), or polynomial regression to LEGO data. The input data frame must contain the columns \code{pieces} and \code{us_retailprice}.
+#' If \code{"polynomial"} is chosen, the user can specify the degree of the polynomial using the \code{polynomial_degree} argument.
+#'
+#' @return An object of class \code{"lego_fit"}. This object contains:
+#' \item{model}{The fitted model object (\code{lm}, \code{loess}, or \code{lm} for polynomial).}
+#' \item{data}{The data frame passed to the function.}
+#' \item{fit_type}{The type of model used (either \code{"lm"}, \code{"loess"}, or \code{"polynomial"}).}
+#'
+#' @importFrom stats lm loess
+#' @export
+#'
+#' @examples
+#' model <- fit(lego_data, "lm")
+#' model2 <- fit(lego_data, "loess")
+#' model3 <- fit(lego_data, "polynomial", polynomial_degree = 2)
+#' model4 <- fit(lego_data, "lm", year_range = c(2018, 2020))
+#' model5 <- fit(lego_data, "polynomial", polynomial_degree = 2, year_range = c(2014,2019))
 fit.legor <- function(obj,
                       fit_type = c("lm", "loess", "polynomial"),
                       polynomial_degree = 3,
@@ -60,6 +101,3 @@ fit.legor <- function(obj,
   class(output) <- c("lego_fit", "listof")
   invisible(output)
 }
-fit(lego_data, "lm")
-fit(lego_data, "lm", year_range = c(2018, 2020))
-
